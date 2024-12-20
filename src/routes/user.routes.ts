e53b6cbfb59d8f4e3a9fdd0c80d06476c4ba1c9d
glenn <70140106+ausgore@users.noreply.github.com>
2024-12-20 16:01:18 +0800

    Updated user endpoints to start with /users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,25 +1,25 @@
 import { FastifyPluginAsync } from 'fastify';
 import { initORM } from '../orm';
 import { User } from '../entities/user/user.entity';
-import { Gear } from '../entities/user/gear.entity';
-import { Skills } from '../entities/user/skills.entity';
 
 const app: FastifyPluginAsync = async (app) => {
   const db = await initORM();
 
-  app.get('/', async (req, res) => {
+  app.get<{ Params: { id: string } }>('/users/:id', async (req, res) => {
+    return await db.em.findOne(User, req.params.id, { populate: ['*'] });
+  });
+
+  app.get('/users', async (req, res) => {
     return await db.em.findAll(User, { populate: ['*'] });
   });
 
-  app.get<{ Params: { id: string } }>('/create/:id', async (req, res) => {
+  app.post<{ Params: { id: string } }>('/users/create/:id', async (req, res) => {
     const user = new User(req.params.id);
-    user.gear = new Gear();
-    user.skills = new Skills();
     await db.em.persistAndFlush(user);
     return user;
   });
 
-  app.get<{ Params: { uid: string, id: number }}>('/:uid/set-offhand/:id', async (req, res) => {    
+  app.get<{ Params: { uid: string, id: number }}>('/users/:uid/set-offhand/:id', async (req, res) => {    
     const { uid, id } = req.params; 
     const user = await db.em.findOne(User, { id: uid });
     if (!user) return res.status(404).send({ message: 'User cannot be found'});
